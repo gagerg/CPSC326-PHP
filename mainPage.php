bed998a4dc808b89834bd7fb55ebfe88987414e2
gagerg <gagerg@outlook.com>
2019-04-30 20:25:08 -0700

    Create mainPage.php

---
+++ mainPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to the CPSC 326 PHP Page!</title>
+</head>
+<body>
+<h1>Welcome to the CPSC 326 PHP Page</h1>
+<label>These are links to varius apps and functions in PHP</label><br>
+
+<a href="linktest2.php?content=buy">Buy products</a><br>
+<a href="linktest2.php?content=browse">Browse for products</a><br>
+<a href="carttest.php?">See How Shopping Carts Work</a><br>
+</body>
+</html>
