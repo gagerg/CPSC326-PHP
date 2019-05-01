c04920cb99513cd3dcd2c7da7d6216e2986982c7
gagerg <gagerg@outlook.com>
2019-05-01 13:00:50 -0700

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -8,7 +8,7 @@
 <label>These are links to varius apps and functions in PHP</label><br>
 
 <a href="linktest2.php?content=buy">Buy products</a><br>
-<a href="linktest2.php?content=browse">Browse for products</a><br>
+<a href="typetest.html">Test Security Such as Email Checking</a><br>
 <a href="carttest.php?">See How Shopping Carts Work</a><br>
 </body>
 </html>
