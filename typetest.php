0c3496f0afdf438727b85cba322ec0525249fe0d
Jackson Ricks <43710876+GoZags@users.noreply.github.com>
2019-05-02 01:29:07 -0700

    Update typetest.php

--- typetest.php
+++ typetest.php
@@ -4,31 +4,37 @@
 <title>Data Type Test</title>
 <style>
 .warning {
-color:red;
+  color:red;
+  font-weight:bold;
+  text-transform:uppercase;
 }
 </style>
+
 </head>
 <body>
-<h1>Form results:</h1>
+<h1>Results:</h1>
 <?php
-$name = htmlspecialchars($_POST['name']);
-$email = htmlspecialchars($_POST['email']);
-$age = htmlspecialchars($_POST['age']);
-echo "<p>Name: $name</p>\n";
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-echo "<p>Email: $email</p>\n";
-} else {
-echo "<p class='warning'>Please enter a valid
-email address</p>\n";
-}
-if (is_numeric($age)) {
-echo "<p>Age: $age</p>\n";
-} else {
-echo "<p class='warning'>Please enter a valid
-age</p>\n";
-}
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $age = htmlspecialchars($_POST['age']);
+  $sanitized = filter_var($name, FILTER_SANITIZE_STRING);
+  echo "<p>Name: $sanitized</p>\n";
+  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "<p>Email: $email</p>\n";
+    } 
+  else {
+    echo "<p class='warning'>Please enter a valid
+    email address</p>\n";
+    }
+  if (filter_var($age, FILTER_VALIDATE_INT)) {
+    echo "<p>Age: $age</p>\n";
+   } 
+  else {
+    echo "<p class='warning'>Please enter a valid
+    age</p>\n";
+  }
 ?>
 <br>
-<a href="typetest.html">Return to form</a>
+<a href="typetest.html">Return to Entry point</a>
 </body>
 </html>
