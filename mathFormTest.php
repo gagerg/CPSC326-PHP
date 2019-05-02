6912b5fef0c3230ae112dbcc7a17d8d5b1b56a95
vrettke <46793517+vrettke@users.noreply.github.com>
2019-05-01 19:56:33 -0700

    Update mathFormTest.php

--- mathFormTest.php
+++ mathFormTest.php
@@ -6,17 +6,112 @@
 <body>
 <h1>Form results:</h1>
 <?php
-$logInput = $_POST['logInput'];
-$logResult = log($logInput);
-$cosInput = $_POST['cosInput'];
-$cosResult = cos($cosInput);
-$hcosInput = $_POST['hcosInput'];
-$hcosResult = cosh($hcosInput);
+$lInput = $_POST['lInput'];
+$tInput = $_POST['tInput'];
+$hInput = $_POST['hInput'];
 
+if (isset($_POST['lfunction'])) 
+{
+	$lfunction = $_POST['lfunction'];
+	if ($lfunction == "exp")
+	{
+		$lResult = exp($lInput);
+	} 
+	else if ($lfunction == "expm1")
+	{
+		$lResult = expm1($lInput);
+	} 
+	else if ($lfunction == "log10")
+	{
+		$lResult = log10($lInput);
+	}
+	else if ($lfunction == "log")
+	{
+		$lResult = log($lInput);
+	}
+}
+else 
+{
+	$lfunction = "not specified";
+}
 
-echo "Natural log result: $logResult <br />";
-echo "Cosine result: $cosResult <br />";
-echo "Hyperbolic cosine result: $hcosResult";
+if (isset($_POST['tfunction'])) 
+{
+	$tfunction = $_POST['tfunction'];
+	if ($tfunction == "acos")
+	{
+		$tResult = acos($tInput);
+	} 
+	else if ($tfunction == "asin")
+	{
+		$tResult = asin($tInput);
+	} 
+	else if ($tfunction == "atan")
+	{
+		$tResult = atan($tInput);
+	}
+	else if ($tfunction == "cos")
+	{
+		$tResult = cos($tInput);
+	}
+	else if ($tfunction == "deg2rad")
+	{
+		$tResult = deg2rad($tInput);
+	} 
+	else if ($tfunction == "rad2deg")
+	{
+		$tResult = rad2deg($tInput);
+	}
+	else if ($tfunction == "sin")
+	{
+		$tResult = sin($tInput);
+	}
+	else if ($tfunction == "tan")
+	{
+		$tResult = tan($tInput);
+	}
+}
+else 
+{
+	$tfunction = "not specified";
+}
+
+if (isset($_POST['hfunction'])) 
+{
+	$hfunction = $_POST['hfunction'];
+	if ($hfunction == "acosh")
+	{
+		$hResult = acosh($hInput);
+	} 
+	else if ($hfunction == "asinh")
+	{
+		$hResult = asinh($hInput);
+	} 
+	else if ($hfunction == "atanh")
+	{
+		$hResult = atanh($hInput);
+	}
+	else if ($hfunction == "cosh")
+	{
+		$hResult = cosh($hInput);
+	}
+	else if ($hfunction == "sinh")
+	{
+		$hResult = sinh($hInput);
+	} 
+	else if ($hfunction == "tanh")
+	{
+		$hResult = hypot($hInput);
+	}
+}
+else 
+{
+	$hfunction = "not specified";
+}
+
+echo "$lfunction($lInput) = $lResult<br />";
+echo "$tfunction($tInput) = $tResult<br />";
+echo "$hfunction($hInput) = $hResult<br />";
 
 ?>
 </body>
